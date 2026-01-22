ead973c9acea282043fa924b160b9b69fca32d28
Rafhael Marsigli <rmarsigli@gmail.com>
2026-01-21 21:55:20 -0300

    chore(cleanup): remove react and unused dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import Icons from 'unplugin-icons/vite';
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://colegioimperatrice.com.br',
-  integrations: [react()],
+  integrations: [],
   image: {
     service: passthroughImageService(),
   },
